f9cac9333c0a387ca1a850056e2f9d5f462ecbbd
tencalink0 <bencax270@gmail.com>
2025-07-06 15:37:37 +0100

    feat: basic boilerplate for project

---
+++ backend/express/src/server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get(/^.*$/, (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
